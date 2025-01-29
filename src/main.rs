905d449356ed113b4fe6c85d50f439f9fce18794
Julian <julian@linkcruit.com>
2025-01-28 22:02:14 -0800

    Working s sub some_function

--- src/main.rs
+++ src/main.rs
@@ -41,8 +41,60 @@ const PC_2_TABLE: [u8; 48] = [
     31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32,
 ];
 
+const S1_TABLE: [u64; 64] = [
+    14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7, 0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11,
+    9, 5, 3, 8, 4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0, 15, 12, 8, 2, 4, 9, 1, 7, 5,
+    11, 3, 14, 10, 0, 6, 13,
+];
+
+const S2_TABLE: [u64; 64] = [
+    15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10, 3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10,
+    6, 9, 11, 5, 0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15, 13, 8, 10, 1, 3, 15, 4, 2,
+    11, 6, 7, 12, 0, 5, 14, 9,
+];
+
+const S3_TABLE: [u64; 64] = [
+    10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8, 13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14,
+    12, 11, 15, 1, 13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7, 1, 10, 13, 0, 6, 9, 8, 7,
+    4, 15, 14, 3, 11, 5, 2, 12,
+];
+
+const S4_TABLE: [u64; 64] = [
+    7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15, 13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12,
+    1, 10, 14, 9, 10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4, 3, 15, 0, 6, 10, 1, 13, 8,
+    9, 4, 5, 11, 12, 7, 2, 14,
+];
+
+const S5_TABLE: [u64; 64] = [
+    2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9, 14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10,
+    3, 9, 8, 6, 4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14, 11, 8, 12, 7, 1, 14, 2, 13,
+    6, 15, 0, 9, 10, 4, 5, 3,
+];
+
+const S6_TABLE: [u64; 64] = [
+    12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11, 10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14,
+    0, 11, 3, 8, 9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6, 4, 3, 2, 12, 9, 5, 15, 10,
+    11, 14, 1, 7, 6, 0, 8, 13,
+];
+
+const S7_TABLE: [u64; 64] = [
+    4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1, 13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12,
+    2, 15, 8, 6, 1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2, 6, 11, 13, 8, 1, 4, 10, 7,
+    9, 5, 0, 15, 14, 2, 3, 12,
+];
+
+const S8_TABLE: [u64; 64] = [
+    13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7, 1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11,
+    0, 14, 9, 2, 7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8, 2, 1, 14, 7, 4, 10, 8, 13,
+    15, 12, 9, 0, 3, 5, 6, 11,
+];
+
 fn check_string_is_ascii_hexdigit(s: String) -> bool {
     return s.chars().all(|c| c.is_ascii_hexdigit());
+}
+
+fn print_u64(label: &str, block: u64) {
+    println!("{}{}", label, format!("{:064b}", block));
 }
 
 fn get_permutated_block<const N: usize>(
@@ -89,6 +141,22 @@ fn split_permutated_key_64(key_64: u64) -> (u64, u64) {
     let right_split_block: u64 = key_64 & RIGHT_SPLIT_KEY_PAD_64;
     return (left_split_block, right_split_block);
 }
+// 0000 000000 000000 111111 000000 000000 000000 000000 000000 000000 000000
+const FIRST_6BIT_IN_48: u64 = 277076930199552;
+// 0000 000000 000000 000000 111111 000000 000000 000000 000000 000000 000000
+const SECOND_6BIT_IN_48: u64 = 4329327034368;
+// 0000 000000 000000 000000 000000 111111 000000 000000 000000 000000 000000
+const THIRD_6BIT_IN_48: u64 = 67645734912;
+// 0000 000000 000000 000000 000000 000000 111111 000000 000000 000000 000000
+const FORTH_6BIT_IN_48: u64 = 1056964608;
+// 0000 000000 000000 000000 000000 000000 000000 111111 000000 000000 000000
+const FIFTH_6BIT_IN_48: u64 = 16515072;
+// 0000 000000 000000 000000 000000 000000 000000 000000 111111 000000 000000
+const SIXTH_6BIT_IN_48: u64 = 258048;
+// 0000 000000 000000 000000 000000 000000 000000 000000 000000 111111 000000
+const SEVENTH_6BIT_IN_48: u64 = 4032;
+// 0000 000000 000000 000000 000000 000000 000000 000000 000000 000000 111111
+const EIGHTH_6BIT_IN_48: u64 = 63;
 
 const BIT_PAD_28: u64 = 268435455;
 fn left_shift_28_bit_pair(left_key: u64, right_key: u64, shift_size: usize) -> (u64, u64) {
@@ -134,20 +202,75 @@ fn get_pc2_permuted_keys(pc_1_keys: [(u64, u64); 16]) -> [u64; 16] {
     return pc_2_keys;
 }
 
+fn get_s_box_row_col(bit_6_block: u64) -> usize {
+    let first_bit = (bit_6_block >> 4) & 2;
+    let last_bit = bit_6_block & 1;
+    let middle_4_bits = (bit_6_block >> 1) & 15;
+    let row = first_bit | last_bit;
+    let col = middle_4_bits;
+    return (row * 16 + col) as usize;
+}
+
 fn some_function(block_32: u64, key: u64) -> u64 {
-    // Block 32 is Correct
-    let mut permutated_block: u64 = 0;
-    for index in 0..48 {
-        let target_bit_index: u8 = E_BIT_SELECTION_TABLE[index] - 1;
-        let right_shift: u8 = 63 - target_bit_index;
-        let bit = (block_32 >> right_shift - 32) & 1;
-        // Check this
-        let new_block_with_bit: u64 = bit << (63 - index - (16));
-        permutated_block |= new_block_with_bit;
-    }
+    let expanded_block = get_permutated_block(block_32, E_BIT_SELECTION_TABLE, 32);
+    println! {"ER0:\n{}", format!("{:064b}", expanded_block)};
     println! {"K1:\n{}", format!("{:064b}", key)};
-    println! {"ER0:\n{}", format!("{:064b}", permutated_block)};
-    println! {"K1+E(R0):\n{}", format!("{:064b}", key ^ permutated_block)};
+    let key_xor_expanded_block = key ^ expanded_block;
+    println! {"K1+E(R0):\n{}", format!("{:064b}", key_xor_expanded_block)};
+
+    let b1 = (key_xor_expanded_block & FIRST_6BIT_IN_48) >> 42;
+    let b1_sub = S1_TABLE[get_s_box_row_col(b1)];
+
+    let b2 = (key_xor_expanded_block & SECOND_6BIT_IN_48) >> 36;
+    let b2_sub = S2_TABLE[get_s_box_row_col(b2)];
+    print_u64("B2\n", b2);
+
+    let b3 = (key_xor_expanded_block & THIRD_6BIT_IN_48) >> 30;
+    let b3_sub = S3_TABLE[get_s_box_row_col(b3)];
+    print_u64("B3\n", b3);
+
+    let b4 = (key_xor_expanded_block & FORTH_6BIT_IN_48) >> 24;
+    let b4_sub = S4_TABLE[get_s_box_row_col(b4)];
+    print_u64("B4\n", b4);
+
+    let b5 = (key_xor_expanded_block & FIFTH_6BIT_IN_48) >> 18;
+    let b5_sub = S5_TABLE[get_s_box_row_col(b5)];
+    print_u64("B5\n", b5);
+
+    let b6: u64 = (key_xor_expanded_block & SIXTH_6BIT_IN_48) >> 12;
+    let b6_sub = S6_TABLE[get_s_box_row_col(b6)];
+    print_u64("B6\n", b6);
+
+    // 010100
+    let b7 = (key_xor_expanded_block & SEVENTH_6BIT_IN_48) >> 6;
+    let b7_sub = S7_TABLE[get_s_box_row_col(b7)];
+    print_u64("B7\n", b7);
+
+    // println!("B7 Row expecting 0: {}", (B7 >> 4) - 1);
+    // println!("B7 Col expecting 3: {}", (B7 & 15) - 1);
+    // println!(
+    //     "ELEMENT: {}",
+    //     get_16_col_array((B7 >> 4) - 1, (B7 & 15) - 1)
+    // );
+    print_u64("B7 SUB\n", b7_sub);
+    // Apparently real answer is 9?
+    println!("B7 SUB expecting 14: {}", b7_sub);
+
+    let b8: u64 = key_xor_expanded_block & EIGHTH_6BIT_IN_48;
+    let b8_sub = S8_TABLE[get_s_box_row_col(b8)];
+    print_u64("B8\n", b8);
+    print_u64("B8 SUB\n", b8_sub);
+
+    let sub = (b1_sub << 28)
+        | (b2_sub << 24)
+        | (b3_sub << 20)
+        | (b4_sub << 16)
+        | (b5_sub << 12)
+        | (b6_sub << 8)
+        | (b7_sub << 4)
+        | (b8_sub);
+    println!("SUB: {}", sub);
+    print_u64("SUB: ", sub);
 
     return 0;
 }
